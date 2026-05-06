17eb6b6dbc240fbee8633e1f3acda26479e43277
dsdsfqswq <a45863782@gmail.com>
2026-05-06 13:00:35 +0500

    latest version

--- dodgeshift/Game1.cs
+++ dodgeshift/Game1.cs
@@ -13,14 +13,17 @@ public class Game1 : Game
     private SpriteBatch _spriteBatch;
 
     private Texture2D _blankTexture;
-    private Rectangle _playerRectangle;
+    private Rectangle _player;
     private List<Rectangle> _fallingBlocks = new();
     private List<Rectangle> _staticBlocks = new();
+    
+    private const int WindowWidth = 800;
+    private const int WindowHeight = 600;
     
     private KeyboardState _previousKeyboardState;
     private float _spawnTimer;
     private float _verticalVelocity;
-    private bool _isGrounded;
+    private bool _isOnTheFloor;
     private Random _randomGenerator = new();
 
     private const int GroundLevelY = 560;
@@ -28,8 +31,12 @@ public class Game1 : Game
     private const float JumpImpulse = -16f;
     private const int MovementSpeed = 6;
     private const int BlockSize = 40;
-    private const float SpawnIntervalSeconds = 0.5f;
+    private const float SpawnIntervalSeconds = 0.3f;
     private const int MaxBlocksOnGround = 60;
+    private int _columns =  WindowWidth / BlockSize;
+    private const int fallingSpeed = 5;
+    
+    private const int CollisionTolerance = 10;
 
     public Game1()
     {
@@ -53,52 +60,75 @@ public class Game1 : Game
         _blankTexture = new Texture2D(GraphicsDevice, 1, 1);
         _blankTexture.SetData(new[] { Color.White });
 
-        _playerRectangle = new Rectangle(400, GroundLevelY, BlockSize, BlockSize);
+        _player = new Rectangle(WindowWidth / 2, GroundLevelY, BlockSize, BlockSize);
     }
 
     protected override void Update(GameTime gameTime)
     {
-        float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        float timePassed = (float)gameTime.ElapsedGameTime.TotalSeconds;
         var currentKeyboardState = Keyboard.GetState();
+        var horizontalMove = 0;
 
-        if (currentKeyboardState.IsKeyDown(Keys.A)) _playerRectangle.X -= MovementSpeed;
-        if (currentKeyboardState.IsKeyDown(Keys.D)) _playerRectangle.X += MovementSpeed;
-
-        _verticalVelocity += GravityForce;
-        _playerRectangle.Y += (int)_verticalVelocity;
-
-        _isGrounded = false;
-        if (_playerRectangle.Y >= GroundLevelY)
+        if (currentKeyboardState.IsKeyDown(Keys.A))
         {
-            _playerRectangle.Y = GroundLevelY;
+            horizontalMove -= MovementSpeed;
+        }
+
+        if (currentKeyboardState.IsKeyDown(Keys.D))
+        {
+            horizontalMove += MovementSpeed;
+        }
+        
+        if (horizontalMove != 0)
+        {
+            int oldX = _player.X;
+            _player.X += horizontalMove;
+            
+            foreach (var staticBlock in _staticBlocks)
+            {
+                if (_player.Intersects(staticBlock))
+                {
+                    _player.X = oldX; 
+                    break;
+                }
+            }
+        }
+        _player.X = Math.Clamp(_player.X, 0, WindowWidth - _player.Width);
+        _verticalVelocity += GravityForce;
+        _player.Y += (int)_verticalVelocity;
+
+        _isOnTheFloor = false;
+        if (_player.Y >= GroundLevelY)
+        {
+            _player.Y = GroundLevelY;
             _verticalVelocity = 0;
-            _isGrounded = true;
+            _isOnTheFloor = true;
         }
 
         foreach (var staticBlock in _staticBlocks)
         {
-            if (_playerRectangle.Intersects(staticBlock))
+            if (_player.Intersects(staticBlock))
             {
-                if (_verticalVelocity >= 0 && _playerRectangle.Bottom <= staticBlock.Top + (_verticalVelocity + 10))
+                if (_verticalVelocity >= 0 && _player.Bottom <= staticBlock.Top + (_verticalVelocity + CollisionTolerance))
                 {
-                    _playerRectangle.Y = staticBlock.Y - _playerRectangle.Height;
+                    _player.Y = staticBlock.Y - _player.Height;
                     _verticalVelocity = 0;
-                    _isGrounded = true;
+                    _isOnTheFloor = true;
                 }
             }
         }
 
-        if (currentKeyboardState.IsKeyDown(Keys.Space) && !_previousKeyboardState.IsKeyDown(Keys.Space) && _isGrounded)
+        if (currentKeyboardState.IsKeyDown(Keys.Space) && !_previousKeyboardState.IsKeyDown(Keys.Space) && _isOnTheFloor)
         {
             _verticalVelocity = JumpImpulse;
         }
 
-        _playerRectangle.X = Math.Clamp(_playerRectangle.X, 0, _graphics.PreferredBackBufferWidth - _playerRectangle.Width);
+        _player.X = Math.Clamp(_player.X, 0, _graphics.PreferredBackBufferWidth - _player.Width);
 
-        _spawnTimer += deltaTime;
+        _spawnTimer += timePassed;
         if (_spawnTimer > SpawnIntervalSeconds)
         {
-            int gridPositionX = _randomGenerator.Next(0, 20) * BlockSize; 
+            int gridPositionX = _randomGenerator.Next(0, _columns) * BlockSize; 
             _fallingBlocks.Add(new Rectangle(gridPositionX, -BlockSize, BlockSize, BlockSize)); 
             _spawnTimer = 0;
         }
@@ -106,12 +136,15 @@ public class Game1 : Game
         for (int i = 0; i < _fallingBlocks.Count; i++)
         {
             var fallingBlock = _fallingBlocks[i];
-            fallingBlock.Y += 5;
+            fallingBlock.Y += fallingSpeed;
             _fallingBlocks[i] = fallingBlock;
 
-            if (fallingBlock.Intersects(_playerRectangle))
+            if (fallingBlock.Intersects(_player))
             {
-                 if (fallingBlock.Bottom < _playerRectangle.Bottom + 10) Exit(); 
+                if (fallingBlock.Bottom < _player.Bottom + CollisionTolerance)
+                {
+                    Exit();
+                } 
             }
 
             bool touchesGround = fallingBlock.Y >= GroundLevelY;
@@ -139,7 +172,7 @@ public class Game1 : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
         _spriteBatch.Begin();
-        _spriteBatch.Draw(_blankTexture, _playerRectangle, Color.Green);
+        _spriteBatch.Draw(_blankTexture, _player, Color.Green);
         foreach (var fallingBlock in _fallingBlocks)
         {
             _spriteBatch.Draw(_blankTexture, fallingBlock, Color.Red);
